6f4f6ade72d952c56ec6776e6c85b9ac624617b4
Mayureshju <juvekarmayuresh37@gmail.com>
2024-08-24 15:56:59 +0530

    get bills pagination

---
+++ src/app/api/getipdbill/route.jsx
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { Ipd } from '../../lib/models';
+import connectToDatabase from '../../lib/utils';
+
+export async function GET(request) {
+    const { searchParams } = new URL(request.url);
+    const page = parseInt(searchParams.get('page')) || 1;
+    try {
+        await connectToDatabase();
+        const limit = 2; 
+        const skip = (page - 1) * limit;
+        const ipdBills = await Ipd.find().skip(skip).limit(limit).exec();
+        const totalRecords = await Ipd.countDocuments();
+        return NextResponse.json({
+            ipdBills,
+            currentPage: page,
+            totalPages: Math.ceil(totalRecords / limit),
+            totalRecords,
+        });
+    } catch (error) {
+        console.error('Error fetching IPD records:', error);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    }
+}
