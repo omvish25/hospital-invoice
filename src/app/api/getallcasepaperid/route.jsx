b0512edd1d5cbab84da8cf9bb387762162edc934
Mayureshju <juvekarmayuresh37@gmail.com>
2024-09-07 07:02:49 +0530

    disable chaching

--- src/app/api/getallcasepaperid/route.jsx
+++ src/app/api/getallcasepaperid/route.jsx
@@ -5,10 +5,17 @@ import { IpdCase } from '../../lib/models';
 export async function GET(request) {
     try {
         await connectToDatabase();
-        const ipdCasePapers = await IpdCase.find()
-        return NextResponse.json({
+        const ipdCasePapers = await IpdCase.find();
+        const response = NextResponse.json({
             data: ipdCasePapers,
         }, { status: 200 });
+
+        // Set cache-control headers to prevent caching
+        response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+        response.headers.set('Pragma', 'no-cache');
+        response.headers.set('Expires', '0');
+
+        return response;
     } catch (error) {
         console.error('Error fetching IPD Case Paper records:', error);
         return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
